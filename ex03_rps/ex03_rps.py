d429e5ac6f991a8be6365df71c8896e39c830c3e
Le Guellanff Quentin <quentin.le-guellanff@universite-paris-saclay.fr>
2021-09-24 16:11:46 +0200

    modif exo3

--- ex03_rps/ex03_rps.py
+++ ex03_rps/ex03_rps.py
@@ -1,51 +1,57 @@
 import random
 
-print("Bienvenue dans le jeu 'Pierre, papier, ciseaux', vous devez devez battre l'ordinateur !")
-userInput = input("Entrez pierre, papier ou ciseaux pour tentez de battre la machine :\n")
-userInput = userInput.lower()
-userInput.replace(" ","")
-
 elements = ["pierre","papier","ciseaux"]
 
-while userInput not in elements:
-    userInput = input("Erreur : entrez correctement le mot papier, feuille ou ciseaux :\n")    
+replay = "oui"
 
-randNumber = random.randint(0,2)
-randElement = elements[randNumber]
-
-while randElement == userInput:
-    print("Vous avez choisi : "+userInput+"\n")
-    print("L'ordinateur à choisi : "+randElement+"\n")
-    print("EGALITE !\n")
+while replay == "oui":
+    
+    print("Bienvenue dans le jeu 'Pierre, papier, ciseaux', vous devez devez battre l'ordinateur !")
     userInput = input("Entrez pierre, papier ou ciseaux pour tentez de battre la machine :\n")
     userInput = userInput.lower()
     userInput.replace(" ","")
+    
     while userInput not in elements:
-        userInput = input("Erreur : entrez correctement le mot papier, feuille ou ciseaux :\n")
+        userInput = input("Entrez correctement le mot pierre, papier ou ciseaux :\n")    
+
     randNumber = random.randint(0,2)
     randElement = elements[randNumber]
 
-if randElement == "pierre" and userInput == "papier":
-    print("Vous avez choisi : "+userInput+"\n")
-    print("L'ordinateur à choisi : "+randElement+"\n")
-    print("vous avez gagné !")
-elif randElement == "pierre" and userInput == "ciseaux":
-    print("Vous avez choisi : "+userInput+"\n")
-    print("L'ordinateur à choisi : "+randElement+"\n")
-    print("vous avez perdu !")
-elif randElement == "papier" and userInput == "ciseaux":
-    print("Vous avez choisi : "+userInput+"\n")
-    print("L'ordinateur à choisi : "+randElement+"\n")
-    print("vous avez gagné !")
-elif randElement == "papier" and userInput == "pierre":
-    print("Vous avez choisi : "+userInput+"\n")
-    print("L'ordinateur à choisi : "+randElement+"\n")
-    print("vous avez perdu !")
-elif randElement == "ciseaux" and userInput == "pierre":
-    print("Vous avez choisi : "+userInput+"\n")
-    print("L'ordinateur à choisi : "+randElement+"\n")
-    print("vous avez gagné !")
-elif randElement == "ciseaux" and userInput == "papier":
-    print("Vous avez choisi : "+userInput+"\n")
-    print("L'ordinateur à choisi : "+randElement+"\n")
-    print("vous avez perdu !")
+    while randElement == userInput:
+        print("Vous avez choisi : "+userInput+"\n")
+        print("L'ordinateur à choisi : "+randElement+"\n")
+        print("EGALITE !\n")
+        userInput = input("Entrez pierre, papier ou ciseaux pour tentez de battre la machine :\n")
+        userInput = userInput.lower()
+        userInput.replace(" ","")
+        while userInput not in elements:
+            userInput = input("Erreur : entrez correctement le mot papier, feuille ou ciseaux :\n")
+        randNumber = random.randint(0,2)
+        randElement = elements[randNumber]
+
+    if randElement == "pierre" and userInput == "papier":
+        print("Vous avez choisi : "+userInput+"\n")
+        print("L'ordinateur à choisi : "+randElement+"\n")
+        print("vous avez gagné !")
+    elif randElement == "pierre" and userInput == "ciseaux":
+        print("Vous avez choisi : "+userInput+"\n")
+        print("L'ordinateur à choisi : "+randElement+"\n")
+        print("vous avez perdu !")
+    elif randElement == "papier" and userInput == "ciseaux":
+        print("Vous avez choisi : "+userInput+"\n")
+        print("L'ordinateur à choisi : "+randElement+"\n")
+        print("vous avez gagné !")
+    elif randElement == "papier" and userInput == "pierre":
+        print("Vous avez choisi : "+userInput+"\n")
+        print("L'ordinateur à choisi : "+randElement+"\n")
+        print("vous avez perdu !")
+    elif randElement == "ciseaux" and userInput == "pierre":
+        print("Vous avez choisi : "+userInput+"\n")
+        print("L'ordinateur à choisi : "+randElement+"\n")
+        print("vous avez gagné !")
+    elif randElement == "ciseaux" and userInput == "papier":
+        print("Vous avez choisi : "+userInput+"\n")
+        print("L'ordinateur à choisi : "+randElement+"\n")
+        print("vous avez perdu !")
+    userInput = ""
+    replay = input("Entrez oui si vous voulez rejouer !\n")
